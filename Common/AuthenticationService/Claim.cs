3e6ee0fbda27e5811052c84dfa4a1a8fb3184efb
aungphyokhinedev <aungphyokhine.dev@gmail.com>
2022-02-25 04:21:28 +0630

    add device id to claim

--- Common/AuthenticationService/Claim.cs
+++ Common/AuthenticationService/Claim.cs
@@ -3,6 +3,7 @@ namespace AuthenticationService;
 public class Claim {
     public string uid {get;set;}
     public string role {get;set;}
+    public string deviceid {get;set;}
     public DateTimeOffset expiration {get;set;}
 
    
